c7cc791bc55508b0374e75e240914030d49a9632
Mohammed Qaid <eng.mo.amri@gmail.com>
2024-05-30 17:44:51 +0300

    added add schedule page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,81 @@
+
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:mosnad_3/views/add_scedule_page.dart';
 
 import 'database/my_database.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+// Be sure to annotate your callback function to avoid issues in release mode on Flutter >= 3.3.0
+@pragma('vm:entry-point')
+alarmHandler() async {
+  try{
+    // TODO check if the alarm period is ended
+    final int id = Isolate.current.hashCode;
+      var androidInitialize =
+      const AndroidInitializationSettings('mipmap/ic_launcher');
+      var initializationsSettings =
+      InitializationSettings(android: androidInitialize);
+      await flutterLocalNotificationsPlugin.initialize(initializationsSettings);
+      String title = "وقت الدراسة";
+      String body = "حان وقت الدراسة";
+      AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+        "mosnad_defualt_channel_id",
+        "mosnad",
+        channelDescription: "This is my channel description",
+        playSound: true,
+        styleInformation: BigTextStyleInformation(body),
+        largeIcon: null,
+        importance: Importance.high,
+        priority: Priority.high,
+        ticker: 'ticker',
+        color: Colors.blue,
+        colorized: true,
+        category: AndroidNotificationCategory.alarm
+      );
+      var notification = NotificationDetails(
+        android: androidNotificationDetails,
+      );
+      await flutterLocalNotificationsPlugin.show(
+          id, title, body, notification);
+    // }
+  }catch(_){}
+}
 void main() async{
+  // Be sure to add this line if initialize() call happens before runApp()
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await AndroidAlarmManager.initialize();
   await MyDatabase.open();
+  //
+  // final receivedIntent = await ReceiveIntent.getInitialIntent();
+  // if (receivedIntent?.action == "android.intent.action.MAIN"){
+  //   print("1");
+  //   final id = receivedIntent?.extra?["id"];
+  //   final paramsExtra = receivedIntent?.extra?["params"];
+  //   Map<String, dynamic> params;
+  //   if (paramsExtra != null){
+  //     // The received params is a string, so we need to convert it into a json map
+  //     params = jsonDecode(paramsExtra);
+  //   }else{
+  //     return;
+  //   }
+  //   print("2");
+  //   // check if this a stop schedule alarm
+  //   if(id >= 1000000){
+  //     await AndroidAlarmManager.cancel(int.parse(params["StudyAlarmID"]));// getting the repeated alarm id, we divide by 100000 coz it was multiplied by 10
+  //     return;
+  //   }
+  //   print("3");
+
   runApp(const MyApp());
+
 }
 
 
@@ -13,13 +84,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>()!.requestNotificationsPermission();
+
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const AddSchedulePage(),
     );
   }
 }
---
+++ lib/view_models/add_schedule_view_model.dart
@@ -0,0 +1,217 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mosnad_3/main.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class AddScheduleViewModel extends GetxController {
+  var saving = false.obs;
+  final formKey = GlobalKey<FormState>();
+  final subjectNameController = TextEditingController();
+  final startDateController = TextEditingController();
+  Rx<DateTime?> startDate = Rx(null);
+  final endDateController = TextEditingController();
+  Rx<DateTime?> endDate = Rx(null);
+  final startTimeController = TextEditingController();
+  final endTimeController = TextEditingController();
+  var repeat = 1.obs;
+  Rx<List<bool>> days = Rx([false, false, false, false, false, false, false]);
+  var reminderTimeValue = '5'.obs;
+  var reminderTimes = [
+    '5',
+    '10',
+    '15',
+  ];
+
+  Future<DateTime?> selectDate(context,{DateTime? start}) async {
+    return await showDatePicker(
+      context: context,
+      initialDate: start??DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+      builder: (context, child) {
+        return Theme(
+          data: ThemeData.light(),
+          child: child!,
+        );
+      },
+    );
+  }
+  Future<TimeOfDay?> selectTime(context) async {
+    return await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.now(),
+        builder: (context, child) {
+          return Theme(
+            data: ThemeData.light(),
+            child: child!,
+          );
+        });
+
+  }
+
+  String getSelectedDays(){
+    List<String> daysNames = ["السبت", "الأحد", "الإثنان", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة"];
+    var selectedDays = "";
+    for(var day in daysNames){
+      if(days.value[daysNames.indexOf(day)]){
+        selectedDays = "$selectedDays[$day]";
+      }
+    }
+    return selectedDays;
+  }
+  void addSchedule() async{
+    if(!formKey.currentState!.validate()){
+      return;
+    }
+    saving.value = true;
+    // Add to local database then
+
+    int scheduleId = 0;// get the id from the database
+    var startDateTime = DateTime(startDate.value!.year,startDate.value!.month,startDate.value!.day,
+        int.parse(startTimeController.text.split(":")[0]), int.parse(startTimeController.text.split(":")[1]));
+    if(repeat.value == 1){
+      AndroidAlarmManager.periodic(const Duration(days: 1),
+          scheduleId*10,// we multiply by 10 to avoid conflicts with specifics days alarms
+          alarmHandler,
+          //                     to make the app reminds the user before the study time
+          startAt: startDateTime.subtract(Duration(minutes: int.parse(reminderTimeValue.value))),
+          allowWhileIdle: true,
+          exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+          wakeup: true,
+          rescheduleOnReboot: true,
+          params: {
+            "Subject": subjectNameController.text,
+          }
+      );
+      // // set an alarm to stop or cancel the alarm at the end date
+      // AndroidAlarmManager.oneShotAt(endDate.value!,
+      //     scheduleId+1000000,// to avoid conflicts with study alarms
+      //     alarmHandler,
+      //     allowWhileIdle: true,
+      //     exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+      //     wakeup: true,
+      //     rescheduleOnReboot: true,
+      //     params: {
+      //       "StudyAlarmID": "${scheduleId*10}",
+      //     }
+      // );
+
+    }else{
+      int day = 0;
+      while(day < 7) {
+        if(days.value[day]) {
+          AndroidAlarmManager.periodic(
+              const Duration(days: 8),
+              (scheduleId*10)+day, // here we jus add the day index
+              alarmHandler,
+              //                               to make the app reminds the user before the study time
+              startAt: startDateTime.next(day).subtract(Duration(minutes: int.parse(reminderTimeValue.value))),
+              allowWhileIdle: true,
+              exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+              wakeup: true,
+              rescheduleOnReboot: true,
+              params: {
+                "Subject": subjectNameController.text,
+              });
+          // // set an alarm to stop or cancel the alarm at the end date
+          // AndroidAlarmManager.oneShotAt(endDate.value!,
+          //     ((scheduleId*10)+day)*1000000,// to avoid conflicts with study alarms
+          //     alarmHandler,
+          //     allowWhileIdle: true,
+          //     exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+          //     wakeup: true,
+          //     rescheduleOnReboot: true,
+          //     params: {
+          //       "StudyAlarmID": "${(scheduleId*10)+day}",
+          //     }
+          // );
+
+        }
+        day++;
+      }
+    }
+    saving.value = false;
+
+  }
+
+  // void testAddSchedule() async{
+  //   saving.value = true;
+  //
+  //   // Add to local database then
+  //
+  //   int scheduleId = 0;// get the id from the database
+  //   var startDateTime = DateTime.now();
+  //   if(repeat.value == 1){
+  //     AndroidAlarmManager.periodic(const Duration(seconds: 15),
+  //         scheduleId*10,// we multiply by 10 to avoid conflicts with specifics days alarms
+  //         alarmHandler,
+  //         startAt: startDateTime,
+  //         allowWhileIdle: true,
+  //         exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+  //         wakeup: true,
+  //         rescheduleOnReboot: true,
+  //         params: {
+  //           "Subject": subjectNameController.text,
+  //         }
+  //     );
+  //     // set an alarm to stop or cancel the alarm at the end date
+  //     AndroidAlarmManager.oneShotAt(startDateTime.add(const Duration(minutes: 2)),
+  //         scheduleId+1000000,// to avoid conflicts with study alarms
+  //         alarmHandler,
+  //         allowWhileIdle: true,
+  //         exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+  //         wakeup: true,
+  //         rescheduleOnReboot: true,
+  //         params: {
+  //           "StudyAlarmID": "${scheduleId*10}",
+  //         }
+  //     );
+  //
+  //   }else{
+  //     int part = 0;
+  //     while(part < 4) {
+  //       if(part % 2 == 1) {
+  //         AndroidAlarmManager.periodic(
+  //             const Duration(minutes: 1),
+  //             (scheduleId*10)+part, // here we jus add the day index
+  //             alarmHandler,
+  //             startAt: startDateTime.add(Duration(seconds: part*15)),
+  //             allowWhileIdle: true,
+  //             exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+  //             wakeup: true,
+  //             rescheduleOnReboot: true,
+  //             params: {
+  //               "Subject": subjectNameController.text,
+  //             });
+  //         // set an alarm to stop or cancel the alarm at the end date
+  //         AndroidAlarmManager.oneShotAt(endDate.value!,
+  //             ((scheduleId*10)+part)*1000000,// to avoid conflicts with study alarms
+  //             alarmHandler,
+  //             allowWhileIdle: true,
+  //             exact: (await Permission.scheduleExactAlarm.request()).isGranted,
+  //             wakeup: true,
+  //             rescheduleOnReboot: true,
+  //             params: {
+  //               "StudyAlarmID": "${(scheduleId*10)+part}",
+  //             }
+  //         );
+  //
+  //       }
+  //       part++;
+  //     }
+  //   }
+  //   saving.value = false;
+  //
+  // }
+}
+// extension to find the next day datetime
+extension DateTimeExtension on DateTime {
+  DateTime next(int day) {
+    return add(
+      Duration(
+        days: (day - weekday) % DateTime.daysPerWeek,
+      ),
+    );
+  }
+}
